d19a6af55fc225067f545dfd61fd2f2edc696369
canaryincoalmine <colerobertson55@gmail.com>
2025-01-25 12:08:21 -0800

    Updated player movement tracking.

--- Code/Player/player.gd
+++ Code/Player/player.gd
@@ -8,24 +8,33 @@ extends CharacterBody2D
 
 func get_input():
 	# Get the user input as a Vector2.
-	return Vector2(
-		(Input.is_action_pressed("right") as int) - (Input.is_action_pressed("left") as int),
-		(Input.is_action_pressed("down") as int) - (Input.is_action_pressed("up") as int)	
-	)
+	#return Vector2(
+		#(Input.is_action_pressed("right") as int) - (Input.is_action_pressed("left") as int),
+		#(Input.is_action_pressed("down") as int) - (Input.is_action_pressed("up") as int)	
+	#)
+	var mouse_position = get_global_mouse_position()
+	var mouse_distance = mouse_position - position
 	
+	if (mouse_position - position).length() > 50:
+		if mouse_distance.length_squared() > 10:
+			look_at(mouse_position)
+		velocity = velocity.lerp(transform.x * SPEED, ACCELERATION)
+	else:
+		velocity = velocity.lerp(Vector2.ZERO, FRICTION)	
+		
 func _physics_process(delta: float) -> void:
 	# Process physics
-	var direction = get_input()
-	if direction.length() > 0:
-		velocity = velocity.lerp(direction.normalized() * SPEED , ACCELERATION)
-	else:
-		velocity = velocity.lerp(Vector2.ZERO, FRICTION)
+	#var direction = get_input()
+	#if direction.length() > 0:
+		#velocity = velocity.lerp(direction.normalized() * SPEED , ACCELERATION)
+	#else:
+		#velocity = velocity.lerp(Vector2.ZERO, FRICTION)
 	
 	#var mouse_position = get_global_mouse_position()
 	#
 	#var direction = (mouse_position - position).normalized()
 	#velocity = direction * SPEED
 	
-	look_at(get_global_mouse_position())
+	get_input()
 	
 	move_and_slide()
