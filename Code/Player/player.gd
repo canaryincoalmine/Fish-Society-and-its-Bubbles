f84801735f8e6d0bf529ab8a9b237dc0c674a881
canaryincoalmine <colerobertson55@gmail.com>
2025-01-25 11:36:56 -0800

    Updated player movement to follow mouse.

--- Code/Player/player.gd
+++ Code/Player/player.gd
@@ -3,20 +3,27 @@ extends CharacterBody2D
 
 @export var SPEED = 200
 @export var FRICTION = 0.04
-@export var ACCELERATION = 0.1
+@export var ACCELERATION = 0.05
 
-func get_input():
-	# Get the user input as a Vector2.
-	return Vector2(
-		(Input.is_action_pressed("right") as int) - (Input.is_action_pressed("left") as int),
-		(Input.is_action_pressed("down") as int) - (Input.is_action_pressed("up") as int)	
-	)
+#func get_input():
+	## Get the user input as a Vector2.
+	#return Vector2(
+		#(Input.is_action_pressed("right") as int) - (Input.is_action_pressed("left") as int),
+		#(Input.is_action_pressed("down") as int) - (Input.is_action_pressed("up") as int)	
+	#)
 	
 func _physics_process(delta: float) -> void:
 	# Process physics
-	var direction = get_input()
-	if direction.length() > 0:
-		velocity = velocity.lerp(direction.normalized() * SPEED , ACCELERATION)
+	#var direction = get_input()
+	#if direction.length() > 0:
+		#velocity = velocity.lerp(direction.normalized() * SPEED , ACCELERATION)
+	#else:
+		#velocity = velocity.lerp(Vector2.ZERO, FRICTION)
+	
+	var direction = get_global_mouse_position()
+	if position.distance_to(direction) > 3:
+		velocity = velocity.lerp((direction - position).normalized() * SPEED, ACCELERATION)
 	else:
 		velocity = velocity.lerp(Vector2.ZERO, FRICTION)
+	
 	move_and_slide()
