24bbc6fa70f148cbab00beca5e4fc2199bd21603
canaryincoalmine <colerobertson55@gmail.com>
2025-01-25 15:31:13 -0800

    Fixed shooting fire rate

--- Code/Player/player.gd
+++ Code/Player/player.gd
@@ -4,10 +4,11 @@ extends CharacterBody2D
 @export var FRICTION = 0.04
 @export var ACCELERATION = 0.05
 
+var reloaded = true
+
 const BULLET_SCENE = preload("res://Code/Projectiles/Player_Bullet.tscn")
 
-#func _on_ready():
-	
+@onready var mouth: Marker2D = $Marker2D
 
 func get_input():
 	var mouse_position = get_viewport().get_mouse_position()
@@ -20,14 +21,20 @@ func get_input():
 	else:
 		velocity = velocity.lerp(Vector2.ZERO, FRICTION)	
 	
-	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+	if Input.is_action_just_pressed("shoot") and reloaded:
 		var direction = mouse_position - position
 		var bullet = BULLET_SCENE.instantiate()
 		get_tree().root.add_child(bullet)
-		bullet.global_position = global_position
+		bullet.global_position = mouth.global_position
 		bullet.rotation = rotation
+		reloaded = false
+		$Timer.start()
 
 func _physics_process(delta: float) -> void:
 	get_input()
 	
 	move_and_slide()
+
+
+func _on_timer_timeout() -> void:
+	reloaded = true
--- Code/Projectiles/player_bullet.gd
+++ Code/Projectiles/player_bullet.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-var SPEED = 500
+var SPEED = 1000
 
 func _process(delta):
 	position += transform.x * SPEED * delta
