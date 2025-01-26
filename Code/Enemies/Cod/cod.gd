54409d9d8b9d29c19bd6e8e656d8b056484be121
canaryincoalmine <colerobertson55@gmail.com>
2025-01-25 18:28:53 -0800

    Baked enemy navigation for level 1.

--- Code/Enemies/Cod/cod.gd
+++ Code/Enemies/Cod/cod.gd
@@ -3,11 +3,11 @@ extends CharacterBody2D
 
 const SPEED = 150.0
 
-@export var player: Node2D
+@export var target_to_chase: Node2D
 @onready var navigation_agent := $NavigationAgent2D as NavigationAgent2D
 @onready var timer := $Timer as Timer
 
 func _physics_process(delta: float) -> void:
-	navigation_agent.target_position = player.global_position
+	navigation_agent.target_position = target_to_chase.global_position
 	velocity = global_position.direction_to(navigation_agent.get_next_path_position()) * SPEED
 	move_and_slide()
