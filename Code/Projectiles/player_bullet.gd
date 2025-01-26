d51e97ab85b979727091aea454fed5e6ef7b78be
slin04 <74470276+slin04@users.noreply.github.com>
2025-01-25 18:59:58 -0800

    bubble pops

--- Code/Projectiles/player_bullet.gd
+++ Code/Projectiles/player_bullet.gd
@@ -2,5 +2,23 @@ extends Area2D
 
 var SPEED = 1000
 
+func _on_ready():
+	$AnimatedSprite2D.play("default")
+
 func _process(delta):
 	position += transform.x * SPEED * delta
+
+
+func _on_area_entered(area):
+	print("area entered")
+
+
+func _on_body_entered(body):
+	$AnimatedSprite2D.play("pop")
+	SPEED = 0
+
+
+
+func _on_animated_sprite_2d_animation_finished():
+	if $AnimatedSprite2D.animation == "pop":
+		queue_free()
