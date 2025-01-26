49674d55618f4e5daff7d27883e1e7175fa3eed2
slin04 <74470276+slin04@users.noreply.github.com>
2025-01-26 14:43:25 -0800

    end screen sound

--- Code/Screens/end_screen.gd
+++ Code/Screens/end_screen.gd
@@ -2,11 +2,9 @@ extends Control
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	$AudioStreamPlayer2D.play()
-	
-
+func _ready():
+	$VictorySound.play()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(delta):
 	pass
